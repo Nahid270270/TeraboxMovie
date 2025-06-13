94667515dbf4ed39e7a84d4684acdc3109327cf9
Nahid270270 <muktaaktar772211@gmail.com>
2025-06-13 19:49:50 +0600

    Delete utils.py

--- utils.py
+++
@@ -1,7 +0,0 @@
-def format_date(date_obj):
-    # তারিখ ফরম্যাট করার লজিক
-    return date_obj.strftime("%Y-%m-%d")
-
-def generate_unique_id():
-    # ইউনিক আইডি জেনারেট করার লজিক
-    return "some_unique_id"
