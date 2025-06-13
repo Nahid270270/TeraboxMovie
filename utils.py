f944df286ec8251b7398b731dc681d430c851788
Nahid270270 <muktaaktar772211@gmail.com>
2025-06-13 19:07:08 +0600

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,7 @@
+def format_date(date_obj):
+    # তারিখ ফরম্যাট করার লজিক
+    return date_obj.strftime("%Y-%m-%d")
+
+def generate_unique_id():
+    # ইউনিক আইডি জেনারেট করার লজিক
+    return "some_unique_id"
