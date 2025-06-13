23c6fcb1a0355ebf17ea10d23c302a0587841c21
Nahid270270 <muktaaktar772211@gmail.com>
2025-06-13 19:50:01 +0600

    Delete routes directory

--- routes/detail.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, render_template
-
-detail_bp = Blueprint('detail', __name__, url_prefix='/detail')
-
-@detail_bp.route('/<int:item_id>')
-def show_detail(item_id):
-    # এখানে item_id ব্যবহার করে ডেটা লোড করার লজিক থাকবে
-    return render_template('detail.html', item_id=item_id)
